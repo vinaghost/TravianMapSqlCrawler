80bf5f497e15ab072d6ea2356873c2da82b86e2b
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-07-16 11:20:08 +0700

    optimize update method

--- VillageCrawler/Extensions/VillageDbContextExtension.cs
+++ VillageCrawler/Extensions/VillageDbContextExtension.cs
@@ -7,117 +7,117 @@ namespace VillageCrawler.Extensions
 {
     public static class VillageDbContextExtension
     {
+        private static readonly DateTime Today = DateTime.Today;
+
         public static async Task UpdateAlliance(this VillageDbContext context, IList<RawVillage> rawVillages, CancellationToken cancellationToken)
         {
             var alliances = rawVillages.GetAlliances();
 
-            var today = DateTime.Today;
-            if (!await context.AlliancesHistory.AnyAsync(x => x.Date == EF.Constant(today), cancellationToken))
+            if (!await context.AlliancesHistory.AnyAsync(x => x.Date == EF.Constant(Today), cancellationToken))
             {
-                var oldAlliances = context.Alliances
-                    .Select(x => new
-                    {
-                        x.Id,
-                        x.PlayerCount,
-                    })
-                    .AsEnumerable()
+                var oldAlliances = await context.Alliances
                     .Select(x => new AllianceHistory
                     {
                         AllianceId = x.Id,
-                        Date = today,
                         PlayerCount = x.PlayerCount,
                     })
-                    .ToList();
+                    .ToListAsync(cancellationToken);
 
-                foreach (var alliance in oldAlliances)
-                {
-                    var exist = alliances.TryGetValue(alliance.AllianceId, out var todayAlliance);
-                    if (!exist) { continue; }
-                    if (todayAlliance is null) { continue; }
-                    alliance.ChangePlayerCount = todayAlliance.PlayerCount - alliance.PlayerCount;
-                }
-
-                await context.BulkInsertAsync(oldAlliances, cancellationToken);
+                var validAlliances = AllianceHistoryHandle(alliances, oldAlliances);
+                await context.BulkInsertAsync(validAlliances, cancellationToken);
             }
 
             await context.BulkMergeAsync(alliances.Values);
+        }
+
+        private static IEnumerable<AllianceHistory> AllianceHistoryHandle(Dictionary<int, Alliance> todayAlliances, IList<AllianceHistory> oldAlliances)
+        {
+            foreach (var oldAlliance in oldAlliances)
+            {
+                oldAlliance.Date = Today;
+
+                var exist = todayAlliances.TryGetValue(oldAlliance.AllianceId, out var todayAlliance);
+                if (exist && todayAlliance is not null)
+                {
+                    oldAlliance.ChangePlayerCount = todayAlliance.PlayerCount - oldAlliance.PlayerCount;
+                }
+                yield return oldAlliance;
+            }
         }
 
         public static async Task UpdatePlayer(this VillageDbContext context, IList<RawVillage> rawVillages, CancellationToken cancellationToken)
         {
             var players = rawVillages.GetPlayers();
 
-            var today = DateTime.Today;
-
-            if (!await context.PlayersHistory.AnyAsync(x => x.Date == EF.Constant(today), cancellationToken))
+            if (!await context.PlayersHistory.AnyAsync(x => x.Date == EF.Constant(Today), cancellationToken))
             {
-                var oldPlayers = context.Players
-                    .Select(x => new
-                    {
-                        x.Id,
-                        x.AllianceId,
-                        x.Population,
-                    })
-                    .AsEnumerable()
+                var oldPlayers = await context.Players
                     .Select(x => new PlayerHistory
                     {
                         PlayerId = x.Id,
-                        Date = today,
                         AllianceId = x.AllianceId,
                         Population = x.Population,
                     })
-                    .ToList();
+                    .ToListAsync(cancellationToken);
 
-                foreach (var player in oldPlayers)
-                {
-                    var exist = players.TryGetValue(player.PlayerId, out var todayPlayer);
-                    if (!exist) { continue; }
-                    if (todayPlayer is null) { continue; }
-                    player.ChangeAlliance = todayPlayer.AllianceId == player.AllianceId;
-                    player.ChangePopulation = todayPlayer.Population - player.Population;
-                }
+                var validPlayers = PlayerHistoryHandle(players, oldPlayers);
 
-                await context.BulkInsertAsync(oldPlayers, cancellationToken);
+                await context.BulkInsertAsync(validPlayers, cancellationToken);
             }
 
             await context.BulkSynchronizeAsync(players.Values, cancellationToken);
+        }
+
+        private static IEnumerable<PlayerHistory> PlayerHistoryHandle(Dictionary<int, Player> todayPlayers, IList<PlayerHistory> oldPlayers)
+        {
+            foreach (var oldPlayer in oldPlayers)
+            {
+                oldPlayer.Date = Today;
+
+                var exist = todayPlayers.TryGetValue(oldPlayer.PlayerId, out var todayPlayer);
+                if (exist && todayPlayer is not null)
+                {
+                    oldPlayer.ChangeAlliance = todayPlayer.AllianceId != oldPlayer.AllianceId;
+                    oldPlayer.ChangePopulation = todayPlayer.Population - oldPlayer.Population;
+                }
+                yield return oldPlayer;
+            }
         }
 
         public static async Task UpdateVillage(this VillageDbContext context, IList<RawVillage> rawVillages, CancellationToken cancellationToken)
         {
             var villages = rawVillages.GetVillages();
 
-            var today = DateTime.Today;
-
-            if (!await context.VillagesHistory.AnyAsync(x => x.Date == EF.Constant(today), cancellationToken))
+            if (!await context.VillagesHistory.AnyAsync(x => x.Date == EF.Constant(Today), cancellationToken))
             {
-                var oldVillages = context.Villages
-                    .Select(x => new
-                    {
-                        x.Id,
-                        x.Population,
-                    })
-                    .AsEnumerable()
+                var oldVillages = await context.Villages
                     .Select(x => new VillageHistory
                     {
                         VillageId = x.Id,
-                        Date = today,
                         Population = x.Population,
                     })
-                    .ToList();
+                    .ToListAsync(cancellationToken);
 
-                foreach (var player in oldVillages)
-                {
-                    var exist = villages.TryGetValue(player.VillageId, out var todayVillage);
-                    if (!exist) { continue; }
-                    if (todayVillage is null) { continue; }
-                    player.ChangePopulation = todayVillage.Population - player.Population;
-                }
-
-                await context.BulkInsertAsync(oldVillages, cancellationToken);
+                var validVillages = VillageHistoryHandle(villages, oldVillages);
+                await context.BulkInsertAsync(validVillages, cancellationToken);
             }
 
             await context.BulkSynchronizeAsync(villages.Values, cancellationToken);
+        }
+
+        private static IEnumerable<VillageHistory> VillageHistoryHandle(Dictionary<int, Village> todayVillages, IList<VillageHistory> oldVillages)
+        {
+            foreach (var oldVillage in oldVillages)
+            {
+                oldVillage.Date = Today;
+
+                var exist = todayVillages.TryGetValue(oldVillage.VillageId, out var todayVillage);
+                if (exist && todayVillage is not null)
+                {
+                    oldVillage.ChangePopulation = todayVillage.Population - oldVillage.Population;
+                }
+                yield return oldVillage;
+            }
         }
     }
 }
