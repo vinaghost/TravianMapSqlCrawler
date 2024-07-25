a3f38006a23176822fa4eaddfdad85e25acebf6a
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-07-25 22:22:27 +0700

    remove default id server

--- VillageCrawler/Entities/Server.cs
+++ VillageCrawler/Entities/Server.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VillageCrawler.Entities
 {
     [Index(nameof(Url))]
     public class Server
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
 
         public string Url { get; set; } = "";
