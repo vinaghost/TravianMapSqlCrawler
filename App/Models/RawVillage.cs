2ff44f5701f8a0312a9a2d21cc4a276331666407
VINAGHOST <nguyentrungnhan87@gmail.com>
2025-05-23 21:40:21 +0700

    Add GetServerDataCommand and RawVillage model
    Introduces the `GetServerDataCommand` class for processing village data from a `StreamReader`, along with a new `RawVillage` record to encapsulate village properties. Includes unit tests to verify the command's functionality, ensuring it correctly parses and returns a list of villages from input data.

---
+++ App/Models/RawVillage.cs
@@ -0,0 +1,20 @@
+﻿namespace App.Models
+{
+    public sealed record RawVillage(
+        int MapId,
+        int X,
+        int Y,
+        int Tribe,
+        int VillageId,
+        string VillageName,
+        int PlayerId,
+        string PlayerName,
+        int AllianceId,
+        string AllianceName,
+        int Population,
+        string Region,
+        bool IsCapital,
+        bool IsCity,
+        bool IsHarbor,
+        int VictoryPoints);
+}
---
+++ TestProject/Commands/GetServerDataCommandTest.cs
@@ -0,0 +1,25 @@
+﻿using App.Commands;
+using Shouldly;
+
+namespace TestProject.Commands
+{
+    public class GetServerDataCommandTest
+    {
+        [Fact]
+        public async Task ReturnsListOfRawVillage()
+        {
+            // Arrange
+            var content = File.OpenRead("TestData/map.sql");
+            var streamReader = new StreamReader(content);
+
+            var handleBehavior = new GetServerDataCommand.HandleBehavior();
+
+            // Act
+            var result = await handleBehavior.HandleAsync(new(streamReader), CancellationToken.None);
+
+            // Assert
+            result.ShouldNotBeEmpty();
+            result.Count.ShouldBe(1827);
+        }
+    }
+}
